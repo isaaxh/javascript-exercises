6dec2619cc281a62422c32c3a537d021eb6ebe9f
Isaac Hussain <isaaclearns58@gmail.com>
2022-05-11 07:31:13 +0300

    repeatString: updated for test

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
+const input = 'hey';
+const repeatTimes = 5;
 
+const repeatString = function(str, repeat) {
+
+    for (let i = 0; i < repeat; i++) {
+        
+        return console.log(str);
+    }
+    
 };
+
+repeatString(input, repeatTimes)
 
 // Do not edit below this line
 module.exports = repeatString;
