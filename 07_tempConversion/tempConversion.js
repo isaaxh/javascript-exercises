f7fcf94a7424fe1d46e36831aa8dd15d3ee5c8c0
Isaac Hussain <isaaclearns58@gmail.com>
2022-05-17 11:48:37 +0300

    All tests passed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(f) {
+  let c = 0;
+  c = parseFloat(((f - 32) * 5/9).toFixed(1)); 
+  return c;
 };
 
-const ctof = function() {
-
+const ctof = function(c) {
+  let f = 0;
+  f = parseFloat((c * 9/5 + 32).toFixed(1));
+  return f;
 };
 
 // Do not edit below this line
