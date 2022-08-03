5bac757dcf1032b688a19f532d91ecc49dbaaecb
Isaac Hussain <isaaclearns58@gmail.com>
2022-08-03 05:49:55 +0300

    Add solutions

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,36 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    // strips punctuation
+    const stripPunc = string.replace(/[^a-z0-9]/gi, '');
 
-};
+    const finalStripPunc = stripPunc.replace(/ /g, '');
+    
+    const finalStrippedStr = finalStripPunc.toLowerCase();
+
+    const splitStr = finalStrippedStr.split('');
+
+    const reversedArray = splitStr.reverse();
+
+    const finalReversedArray = reversedArray.join('');
+
+    if (finalStrippedStr === finalReversedArray) {
+        return true;
+    } else {
+        return false;
+    }
+
+
+//  const length = lowercasedStr.length;
+
+//     for (let i = 0; i < length/2; i++) {
+
+//         if (string[i] !== string[length - 1 -i]) {
+//             return false;
+//         } else {
+//             return true;
+//         };
+//     };
+// };
+}
 
 // Do not edit below this line
 module.exports = palindromes;
