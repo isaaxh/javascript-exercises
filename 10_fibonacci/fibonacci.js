706a5aabad8b9a9a820976ccbd7b0b218d9e2c70
Isaac Hussain <isaaclearns58@gmail.com>
2022-08-03 17:00:33 +0300

    Add solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let x = 0;
+    let y = 1;
+    let z;
+    let finalNum = num;
 
+    if (isNaN(num)) {
+        finalNum = pasrseInt(num);
+    }
+
+    if (finalNum < 0) return 'OOPS';
+   
+    for (let i = 1; i < finalNum; ++i) {
+        z = x + y;
+        x = y;
+        y = z;
+    }
+    return y; 
 };
 
 // Do not edit below this line
